0b12c3654c31a4b26e0e3814609c91023469f25c
stylebyabdul <hayzedenterprise07@gmail.com>
2022-11-30 03:14:24 +0000

    if else loop function

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import radom
+number = radom.randint(-10, 10)
+if number > 0:
+print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
+else:
+    print("{} is negative".format(number))
