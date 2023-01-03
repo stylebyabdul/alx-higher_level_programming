29b742aec6c09cd377e4e28b8cc71cf7e90e962f
stylebyabdul <hayzedenterprise07@gmail.com>
2023-01-03 17:36:16 +0000

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 3-rectangle.py
-
+# Brennan D Baraban <375@holbertonschool.com>
 """Defines a Rectangle class."""
 
 
@@ -9,15 +9,12 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-
         self.width = width
         self.height = height
-
 
     @property
     def width(self):
@@ -32,7 +29,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
+    @property
     def height(self):
         """Get/set the height of the Rectangle."""
         return self.__height
